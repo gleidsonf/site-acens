6541e87708dbec5189e03e6e9fc32e4007ecf9ed
matheus-santos-araujo <matuf2008@hotmail.com>
2017-10-18 00:54:14 -0300

    Adicionando 1 versão do newsletter e blog(front)

---
+++ static/js/custom.js
@@ -0,0 +1,29 @@
+$(document).on('submit', '#form_contact', function(e) {
+  e.preventDefault();
+
+  jQuery.ajax({
+    type: 'POST',
+    URL: '/sendemail.php/',
+    data: {
+      name: jQuery('#id_email').val(),
+      email: jQuery('#id_email').val(),
+      subject: jQuery('#id_email').val(),
+      message: jQuery('#id_email').val(),
+    },
+    success: function() {
+        console.log('It\'s work!!');
+
+        jQuery.notify('Em breve entaremos em contato!!', {
+        	allow_dismiss: false,
+        	placement: {
+        		from: 'bottom',
+        		align: 'left'
+        	}
+        });
+        jQuery('#form_contact')[0].reset();
+    },
+    error: function() {
+      console.log('or not');
+    }
+  });
+});
